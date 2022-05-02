5c44340aec6449eabbf53a320417875141948513
Qdesnitsa <lenkasid@tut.by>
2022-05-02 22:20:43 +0300

    browser

--- src/by/teachmeskills/lesson13_browser/BrowserHistory.java
+++ src/by/teachmeskills/lesson13_browser/BrowserHistory.java
@@ -24,7 +24,7 @@ public class BrowserHistory {
   }
 
   public String back(int steps) {
-    if (steps >= list.size()) {
+    if (steps >= currentPosition) {
       currentPosition = 0;
       return homepage;
     } else {
@@ -43,17 +43,72 @@ public class BrowserHistory {
     }
   }
 
+
   public static void main(String[] args) {
-    BrowserHistory browserHistory = new BrowserHistory("1");
-    browserHistory.visit("2");
-    browserHistory.visit("3");
-    browserHistory.visit("4");
-    browserHistory.visit("5");
-    browserHistory.back(2);
-    browserHistory.visit("6");
-    browserHistory.forward(1);
-    browserHistory.visit("7");
+    BrowserHistory browserHistory = new BrowserHistory("fmbte.com");//
+    browserHistory.visit("gtbcpfz.com");
+    browserHistory.visit("lkw.com");
+    browserHistory.visit("fosekfi.com");
+
+    browserHistory.back(3);
+    browserHistory.forward(2);
+
+    browserHistory.visit("wtub.com");
+    browserHistory.visit("cymzd.com");
+    browserHistory.visit("pnep.com");
+    browserHistory.visit("cwqnss.com");
+
+    browserHistory.back(5);
+    browserHistory.forward(4);
+
+    browserHistory.visit("gzhsy.com");
+
+    browserHistory.forward(8);
+
+    browserHistory.visit("tiuj.com");
+    browserHistory.visit("zkl.com");
+    browserHistory.visit("ypei.com");
+    browserHistory.visit("uctrp.com");
+
+    browserHistory.forward(5);
+    browserHistory.back(3);
+
+    browserHistory.visit("wo.com");
+
+    browserHistory.back(7);
+
+    browserHistory.visit("rwqe.com");
+    browserHistory.visit("rilu.com");
+
     browserHistory.back(10);
+    browserHistory.forward(1);
+    browserHistory.forward(7);
+
+    browserHistory.visit("uaz.com");
+    browserHistory.visit("em.com");
+
+    browserHistory.forward(4);
+
+    browserHistory.visit("qci.com");
+    browserHistory.visit("clsk.com");
+    browserHistory.visit("mebuat.com");
+    browserHistory.visit("vozvxqc.com");
+    browserHistory.visit("ejiheyj.com");
+
+    browserHistory.back(8);
+
+    browserHistory.visit("bvkvzr.com");
+    browserHistory.visit("zlfrqt.com");
+
+    browserHistory.back(4);
+    browserHistory.back(2);
+    browserHistory.forward(3);
+    browserHistory.back(6);
+
+    browserHistory.visit("xnzy.com");
+    
+    browserHistory.back(9);
+
     System.out.println(list);
     System.out.println(list.get(currentPosition));
   }
