5e7dbef6446f4131aa29855bda5b7dc52416eb68
Qdesnitsa <lenkasid@tut.by>
2022-05-16 04:10:15 +0300

    DOM

--- src/by/teachmeskills/lesson16_xml_json/dom/main/MainDOM.java
+++ src/by/teachmeskills/lesson16_xml_json/dom/main/MainDOM.java
@@ -11,8 +11,6 @@ import org.xml.sax.SAXException;
 public class MainDOM {
   private static String fileFrom = "src/by/teachmeskills/lesson16_xml_json/resources/books.xml";
   private static String fileTo = "src/by/teachmeskills/lesson16_xml_json/dom/entity/output_xml/books.xml";
-  private static String namespace = "http://www.teachmeskills.by/books";
-  private static String prefix = "b";
 
   public static void main(String[] args)
       throws IOException, SAXException, ParserConfigurationException, TransformerException {
